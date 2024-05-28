7fb5efddd099c1bf8cd2bfea6e8d37d46a7c46b4
miguel000127@gmail.com <miguel000127@gmail.com>
2024-05-28 15:27:55 -0500

    # add DepartamentService
    dsc: add DepartmentService file to manage Departments request

---
+++ backendEmployeModule/Services/Implementation/DepartmentService.cs
@@ -0,0 +1,32 @@
+﻿using backendEmployeModule.Models;
+using backendEmployeModule.Services.Contract;
+using Microsoft.EntityFrameworkCore;
+
+namespace backendEmployeModule.Services.Implementation
+{
+    public class DepartmentService : IDepartmentService
+    {
+		private BackendEmployesModuleContext _backendEmployesModuleContext;
+
+        public DepartmentService(BackendEmployesModuleContext backendEmployesModuleContext)
+        {
+            _backendEmployesModuleContext = backendEmployesModuleContext;
+        }
+
+        public async Task<List<Department>> GetDepartments()
+        {
+			try
+			{
+				List<Department> list = new List<Department>();
+                list = await _backendEmployesModuleContext.Departments.ToListAsync();
+                return list;
+
+			}
+			catch (Exception ex)
+			{
+
+				throw ex;
+			}
+        }
+    }
+}
