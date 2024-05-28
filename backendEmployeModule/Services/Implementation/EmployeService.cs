ebeea8349168023bafbffa5a7a227a01a6d8f392
miguel000127@gmail.com <miguel000127@gmail.com>
2024-05-28 15:46:53 -0500

    # add EmployeService
    dsc: interacts with a database through BackendEmployesModuleContext, which is an Entity Framework context. Each method of the EmployeService class

---
+++ backendEmployeModule/Services/Implementation/EmployeService.cs
@@ -0,0 +1,99 @@
+﻿using backendEmployeModule.Models;
+using backendEmployeModule.Services.Contract;
+using Microsoft.EntityFrameworkCore;
+
+namespace backendEmployeModule.Services.Implementation
+{
+    public class EmployeService : IEmployeService
+    {
+        private BackendEmployesModuleContext _backendEmployesModuleContext;
+
+        public EmployeService(BackendEmployesModuleContext backendEmployesModuleContext)
+        {
+            _backendEmployesModuleContext = backendEmployesModuleContext;   
+        }
+
+        public async Task<Employe> addEmploye(Employe employe)
+        {
+            try
+            {
+                _backendEmployesModuleContext.Employes.Add(employe);
+               await _backendEmployesModuleContext.SaveChangesAsync();
+                return employe;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<bool> deleteEmploye(Employe employe)
+        {
+            try
+            {
+                _backendEmployesModuleContext.Employes.Remove(employe);
+                await _backendEmployesModuleContext.SaveChangesAsync() ;
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<List<Employe>> GetAll()
+        {
+            try
+            {
+                List<Employe> employes = new List<Employe>();
+                employes = await _backendEmployesModuleContext.Employes.Include(d=>d.IdDeptNavigation).ToListAsync();
+                return employes;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<Employe> GetEmploye(int idEmploye)
+        {
+            try
+            {
+
+                Employe? find = new Employe();
+                find = await _backendEmployesModuleContext.Employes.Include(dpt =>dpt.IdDeptNavigation)
+                    .Where(e=>e.Id == idEmploye).FirstOrDefaultAsync();
+                return find;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<bool> updateEmploye(Employe employe)
+        {
+            try
+            {
+                _backendEmployesModuleContext.Employes.Update(employe);
+                await _backendEmployesModuleContext.SaveChangesAsync();
+                return true;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
