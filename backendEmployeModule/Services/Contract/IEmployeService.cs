a382b892c370e5d1a8c3c5a68ea28fa41581f75d
miguel000127@gmail.com <miguel000127@gmail.com>
2024-05-28 15:10:21 -0500

    # add IEmployeService
    dsc: Add IEmployeService file to manage Employes methods

---
+++ backendEmployeModule/Services/Contract/IEmployeService.cs
@@ -0,0 +1,13 @@
+﻿using backendEmployeModule.Models;
+namespace backendEmployeModule.Services.Contract
+{
+    public interface IEmployeService
+    {
+        Task<List<Employe>> GetAll();
+        Task<Employe> GetEmploye(int idEmploye);
+        Task<Employe> addEmploye(Employe employe);
+        Task <bool> updateEmploye(Employe employe);
+        Task <bool> deleteEmploye(Employe employe);
+
+    }
+}
