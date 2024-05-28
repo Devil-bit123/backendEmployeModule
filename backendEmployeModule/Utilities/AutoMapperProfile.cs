158b926abb792fa09519311a2a82850a759d6502
miguel000127@gmail.com <miguel000127@gmail.com>
2024-05-28 16:44:03 -0500

    # create EmployeDTO
    dsc: add empoyeDTO file to creat a visual object for send to frontEnd Application

---
+++ backendEmployeModule/Utilities/AutoMapperProfile.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using backendEmployeModule.DTOS;
+using backendEmployeModule.Models;
+using System.Globalization;
+
+namespace backendEmployeModule.Utilities
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+
+            #region Departments
+            CreateMap<Department, DepartmentDTO>().ReverseMap();
+            #endregion
+
+            #region Employes
+
+            /*Method to map Employes from Employe (Model)
+             * To EmployeDTO
+             * 
+             */
+
+            CreateMap<Employe, EmployeDTO>()
+                .ForMember(destiny => destiny.NameDept,
+                opt => opt.MapFrom(origin => origin.IdDeptNavigation.Name)
+                )
+                .ForMember(destiny => destiny.ContractDate,
+                opt => opt.MapFrom(origin => origin.ContractDate.Value.ToString("yyyy-MM-dd"))
+                );
+
+            CreateMap<EmployeDTO, Employe>()
+                .ForMember(destiny => destiny.IdDeptNavigation,
+                opt => opt.Ignore()
+                )
+                .ForMember(destiny => destiny.ContractDate,
+                opt => opt.MapFrom(origin => DateTime.ParseExact(origin.ContractDate,"dd/MM/yyyy", CultureInfo.InvariantCulture))
+                );
+            #endregion
+
+
+        }
+    }
+}
