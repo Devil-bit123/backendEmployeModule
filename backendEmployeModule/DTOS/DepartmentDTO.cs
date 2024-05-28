6efe797242b1574c839bb9963e3b735512f7e57f
miguel000127@gmail.com <miguel000127@gmail.com>
2024-05-28 16:09:22 -0500

    # add DepartmentDTO
    dsc: add DepartmentDTO class to create visualization object for send to FrontEnd application

--- backendEmployeModule/DTOS/DepartmentDTO.cs
+++ backendEmployeModule/DTOS/DepartmentDTO.cs
@@ -2,5 +2,10 @@
 {
     public class DepartmentDTO
     {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
     }
 }
